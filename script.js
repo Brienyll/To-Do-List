48d555dad73a94669af3ea715dcd0fffeee10f7b
Brienyll <brienyll29@gmail.com>
2016-12-15 14:49:43 -0800

    added click function

--- script.js
+++ script.js
@@ -55,3 +55,10 @@ var todoList = {
       this.displayTodos();
   }
 };
+  
+var displayTodosButton = document.getElementById('displayTodosButton');
+console.log(displayTodosButton);
+
+displayTodosButton.addEventListener('click', function() {
+  todoList.displayTodos();
+});
