2f9e46941bcb0d43381d9cff7e099cc45e3d4d4f
Brienyll <brienyll29@gmail.com>
2016-12-28 15:15:52 -0800

    added view element
    added li element for each todo item

--- script.js
+++ script.js
@@ -87,3 +87,14 @@ var handlers = {
     toggleCompletedPositionInput.value = '';
   }
 };
+
+var view = {
+  displayTodos: function() {
+    var todosUl = document.querySelector('ul');
+    todosUl.innerHTML = '';
+    for (var i=0; i < todoList.todos.length; i++) {
+      var todoLi = document.createElement('li');
+      todosUl.appendChild(todoLi);
+    }
+  }
+};
