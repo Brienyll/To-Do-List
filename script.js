0b3c5e28844932d17f0bf2a9426163433b277a89
Brienyll <brienyll29@gmail.com>
2017-01-07 16:10:17 -0800

    added Delete buttons access to todo Id

--- script.js
+++ script.js
@@ -107,3 +107,8 @@ var view = {
     deleteButton.className = 'deleteButton';
     return deleteButton;
 };
+
+var todosUl = document.querySelector('ul');
+todosUl.addEventListener('click', function(event){
+  console.log(event);
+});
